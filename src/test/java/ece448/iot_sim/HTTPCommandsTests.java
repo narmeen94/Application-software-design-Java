ca83ab5fc375ba02f21c27a8ffacce9bbc740c8c
narmeen aamir <naamir@hawk.iit.edu>
2022-02-14 22:17:13 -0600

    second test case passed

--- src/test/java/ece448/iot_sim/HTTPCommandsTests.java
+++ src/test/java/ece448/iot_sim/HTTPCommandsTests.java
@@ -1,6 +1,6 @@
 package ece448.iot_sim;
 
-import ece448.iot_sim.http_server.RequestHandler;
+//import ece448.iot_sim.http_server.RequestHandler;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -37,5 +37,29 @@ public void testListAllPlugs() {
     String pluglist="<html><body><p><a href='/a'>a</a></p><p><a href='/b.100'>b.100</a></p><p><a href='/cc'>cc</a></p><p><a href='/dddd'>dddd</a></p></body></html>";
     assertEquals(HTTPCommands.listPlugs(),pluglist);
 }
+@Test
+public void testplugNotSpecified() {
+    List<PlugSim> plugs = new ArrayList<PlugSim>();
+    PlugSim plug1=new PlugSim("a");
+    PlugSim plug2=new PlugSim("b.100");
+    PlugSim plug3=new PlugSim("cc");
+    PlugSim plug4=new PlugSim("dddd");
+
+    plugs.add(plug1);
+    plugs.add(plug2);
+    plugs.add(plug3);
+    plugs.add(plug4);
+
+    
+    HTTPCommands HTTPCommands=new HTTPCommands(plugs);
+    Map<String, String> params=new TreeMap<>();
+    
+    params.put("action","on");
+    String path="/";
+    String output=HTTPCommands.handleGet(path,params);
+    String pluglist="<html><body><p><a href='/a'>a</a></p><p><a href='/b.100'>b.100</a></p><p><a href='/cc'>cc</a></p><p><a href='/dddd'>dddd</a></p></body></html>";
+    assertEquals(output,pluglist);
+}
+
 }
     
