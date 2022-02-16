c8e72b5fa2e9fa7c56b7af192d234dd19580a18d
narmeen aamir <naamir@hawk.iit.edu>
2022-02-15 19:23:15 -0600

    updated test

--- src/test/java/ece448/iot_sim/HTTPCommandsTests.java
+++ src/test/java/ece448/iot_sim/HTTPCommandsTests.java
@@ -122,5 +122,33 @@ public void testActionOn() {
 
     assertEquals(HTTPCommands.report(plug2),HTTPCommands.handleGet(path,params));
 }
+
+@Test
+public void testActionOff() {
+    List<PlugSim> plugs = new ArrayList<PlugSim>();
+    PlugSim plug1=new PlugSim("a");
+    PlugSim plug2=new PlugSim("b.100");
+    PlugSim plug3=new PlugSim("cc");
+    PlugSim plug4=new PlugSim("dddd");
+
+    plugs.add(plug1);
+    plugs.add(plug2);
+    plugs.add(plug3);
+    plugs.add(plug4);
+
+    
+    HTTPCommands HTTPCommands=new HTTPCommands(plugs);
+    Map<String, String> params=new TreeMap<>();
+    
+    params.put("action","off");
+    String path="/b.100";
+    HTTPCommands.handleGet(path,params);
+    //String pluglist="<html><body><p><a href='/a'>a</a></p><p><a href='/b.100'>b.100</a></p><p><a href='/cc'>cc</a></p><p><a href='/dddd'>dddd</a></p></body></html>";
+    plug2.switchOff();
+    plug2.measurePower();
+    plug2.getPower();
+
+    assertEquals(HTTPCommands.report(plug2),HTTPCommands.handleGet(path,params));
+}
 }
     
