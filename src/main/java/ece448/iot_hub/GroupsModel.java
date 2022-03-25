bd3caa652225470887094e6db399daa29b633bc7
narmeen aamir <naamir@hawk.iit.edu>
2022-03-25 18:47:25 -0500

    almost

--- src/main/java/ece448/iot_hub/GroupsModel.java
+++ src/main/java/ece448/iot_hub/GroupsModel.java
@@ -1,46 +1,46 @@
-package ece448.iot_hub;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+// package ece448.iot_hub;
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.HashSet;
+// import java.util.List;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.springframework.stereotype.Component;
+// import org.eclipse.paho.client.mqttv3.MqttClient;
+// import org.springframework.stereotype.Component;
 
-import ece448.grading.GradeP3.MqttController;
+// import ece448.grading.GradeP3.MqttController;
 
-@Component
-public class GroupsModel {
+// @Component
+// public class GroupsModel {
 
-     //i have added mqttclient here
+//      //i have added mqttclient here
 
-    // private MqttController mqttController;
+//     // private MqttController mqttController;
 
-	//public GroupsModel(MqttController mqttController)
-     //{
-         //this.mqttController=mqttController;
-     //}
-//till here.
+// 	//public GroupsModel(MqttController mqttController)
+//      //{
+//          //this.mqttController=mqttController;
+//      //}
+// //till here.
 
-//groups=plugs
-	private HashMap<String, HashSet<String>> groups = new HashMap<>();
+// //groups=plugs
+// 	private HashMap<String, HashSet<String>> groups = new HashMap<>();
 	
 
-	synchronized public List<String> getGroups() { //getPlugs
-		return new ArrayList<>(groups.keySet());  //only the names of theplugs will be returned by keyset()
-	}
+// 	synchronized public List<String> getGroups() { //getPlugs
+// 		return new ArrayList<>(groups.keySet());  //only the names of theplugs will be returned by keyset()
+// 	}
 
-	synchronized public List<String> getGroupMembers(String group) {//groupmembers=plugNames
-		HashSet<String> members = groups.get(group);                 //group=plug
-		return (members == null)? new ArrayList<>(): new ArrayList<>(members);
-    }
- //i dont think these are needed for now
-    synchronized public void setGroupMembers(String group, List<String> members) {
-		groups.put(group, new HashSet<>(members));
-	}
+// 	synchronized public List<String> getGroupMembers(String group) {//groupmembers=plugNames
+// 		HashSet<String> members = groups.get(group);                 //group=plug
+// 		return (members == null)? new ArrayList<>(): new ArrayList<>(members);
+//     }
+//  //i dont think these are needed for now
+//     synchronized public void setGroupMembers(String group, List<String> members) {
+// 		groups.put(group, new HashSet<>(members));
+// 	}
 
-	synchronized public void removeGroup(String group) {
-		groups.remove(group);
-	}
-}
+// 	synchronized public void removeGroup(String group) {
+// 		groups.remove(group);
+// 	}
+// }
 
