a1088bfc554d9f28a0bd620057fc55e31d48bd3f
narmeen aamir <naamir@hawk.iit.edu>
2022-04-07 13:28:52 -0500

    try

--- src/main/java/ece448/iot_hub/GroupsModel.java
+++ src/main/java/ece448/iot_hub/GroupsModel.java
@@ -1,13 +1,23 @@
 package ece448.iot_hub;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import ece448.grading.GradeP3.MqttController;
 
 import org.springframework.stereotype.Component;
 @Component
  public class GroupsModel {
+	
+	private MqttController mqttController;
+
+	public GroupsModel(MqttController mqttController) {
+		
+		this.mqttController=mqttController;
+	}
+	
 	private HashMap<String, HashSet<String>> groups = new HashMap<>(); 
 
 	synchronized public List<String> getGroups() {
@@ -18,6 +28,41 @@ import org.springframework.stereotype.Component;
 		return (members == null)? new ArrayList<>(): new ArrayList<>(members);
 	}
 
+	synchronized public String getGroupState(String group) {
+		ArrayList<String> memstate=new ArrayList<>();
+		int count=0;
+		String state="unknown";
+		for (String member:getGroupMembers(group))
+		{
+			memstate.add(mqttController.getState(group));
+	        
+			
+			if ((mqttController.getState(member)).equals("on"))
+			{
+				count=count+1;
+
+			}
+			else if ((mqttController.getState(member)).equals("off"))
+			{
+				count=0;
+			}
+			
+			
+		}
+		if (count==memstate.size())
+		{
+		    state="on";
+		}
+		else if (count==0)
+		{
+			state= "off";
+		}
+		
+		return state;
+	}
+
+
+	
 	synchronized public void setGroupMembers(String group, List<String> members) {
 		groups.put(group, new HashSet<>(members));
 	}
--- src/main/java/ece448/iot_hub/GroupsResource.java
+++ src/main/java/ece448/iot_hub/GroupsResource.java
@@ -1,13 +1,117 @@
-package ece448.iot_hub;
+ package ece448.iot_hub;
+
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+// import java.util.TreeMap;
+// import java.util.concurrent.SynchronousQueue;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+// import ece448.grading.GradeP3.MqttController;
+// @RestController
+// public class GroupsResource {
+
+//      private final GroupsModel groups;
+// 	 private MqttController mqttController;
+     
+
+// 	public GroupsResource(GroupsModel groups,MqttController mqttController) {
+//         this.groups = groups;
+//         this.mqttController=mqttController;
+// 	}
+	
+// 	@GetMapping("/api/groups")
+// 	public Collection<Object> getGroups() throws Exception {
+// 		ArrayList<Object> ret = new ArrayList<>();
+// 		for (String group: groups.getGroups()) {
+// 			ret.add(makeGroup(group));
+// 		}
+// 		logger.info("Groups: {}", ret);
+// 		return ret;
+// 	 }
+
+//     @GetMapping("/api/groups/{group}")
+// 	public Object getGroup(
+// 		@PathVariable("group") String group,
+// 		@RequestParam(value = "action", required = false) String action) {
+// 		// if (action == null) {
+// 		// 	Object ret = makeGroup(group);
+// 		// 	logger.info("Group {}: {}", group, ret);
+// 		// 	return ret;
+// 		// }
+// 		if (action!=null)
+// 		{
+//             if(action.equals("on")||action.equals("off")||action.equals("toggle")){
+// 				HashMap<String, Object> ret = new HashMap<>();
+// 		        ret.put("name", group);
+// 		         HashMap<String,String> mem=new HashMap<>();
+// 		        for (String member:groups.getGroupMembers(group))
+// 			    {
+// 					mqttController.publishAction(member, action);
+// 				//mem.put("name",member);
+// 				//mem.put("state",mqttController.getState(member));
+// 				mem.put(member,mqttController.getState(member));
+				
+// 			    }
+// 		        ret.put("members",mem);
+		        
+
+//                //mqttController.publishAction(plugName, action);
+//             }
+//         }
+//         // modify code below to control plugs by publishing messages to MQTT broker
+// 		List<String> members = groups.getGroupMembers(group);
+// 		logger.info("Group {}: action {}, {}", group, action, members);
+// 		//return null;
+// 		Object ret = makeGroup(group);
+// 		return ret;
+// 	}
+//     @PostMapping("/api/groups/{group}")
+// 	public void createGroup(
+// 		@PathVariable("group") String group,
+// 		@RequestBody List<String> members) {
+// 		groups.setGroupMembers(group, members);
+// 		logger.info("Group {}: created {}", group, members);
+//     }
+//     @DeleteMapping("/api/groups/{group}")
+// 	public void removeGroup(
+// 		@PathVariable("group") String group) {
+// 		groups.removeGroup(group);
+// 		logger.info("Group {}: removed", group);
+// 	}
+//     protected Object makeGroup(String group) {
+// 		// modify code below to include plug states
+// 		HashMap<String, Object> ret = new HashMap<>();
+// 		ret.put("name", group);
+// 		HashMap<String,String> mem=new HashMap<>();
+// 		for (String member:groups.getGroupMembers(group))
+// 			{
+// 				//mem.put("name",member);
+// 				//mem.put("state",mqttController.getState(member));
+// 				mem.put(member,mqttController.getState(member));
+				
+// 			}
+// 		ret.put("members",mem);
+// 		return ret;
+// 	}
+
+// 		private static final Logger logger = LoggerFactory.getLogger(GroupsResource.class);	
+// }
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.concurrent.SynchronousQueue;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,17 +121,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import ece448.grading.GradeP3.MqttController;
 @RestController
 public class GroupsResource {
 
-     private final GroupsModel groups;
-	 private MqttController mqttController;
-     
+	private final GroupsModel groups;
+	private MqttController mqttController;
 
 	public GroupsResource(GroupsModel groups,MqttController mqttController) {
-        this.groups = groups;
-        this.mqttController=mqttController;
+		this.groups = groups;
+		this.mqttController=mqttController;
 	}
 	
 	@GetMapping("/api/groups")
@@ -38,9 +142,9 @@ public class GroupsResource {
 		}
 		logger.info("Groups: {}", ret);
 		return ret;
-	 }
+	}
 
-    @GetMapping("/api/groups/{group}")
+	@GetMapping("/api/groups/{group}")
 	public Object getGroup(
 		@PathVariable("group") String group,
 		@RequestParam(value = "action", required = false) String action) {
@@ -52,58 +156,41 @@ public class GroupsResource {
 		if (action!=null)
 		{
             if(action.equals("on")||action.equals("off")||action.equals("toggle")){
-				HashMap<String, Object> ret = new HashMap<>();
-		        ret.put("name", group);
-		         HashMap<String,String> mem=new HashMap<>();
+
 		        for (String member:groups.getGroupMembers(group))
 			    {
-					mqttController.publishAction(member, action);
-				//mem.put("name",member);
-				//mem.put("state",mqttController.getState(member));
-				mem.put(member,mqttController.getState(member));
-				
+					mqttController.publishAction(member, action);				
 			    }
-		        ret.put("members",mem);
-		        
-
-               //mqttController.publishAction(plugName, action);
             }
         }
-        // modify code below to control plugs by publishing messages to MQTT broker
-		List<String> members = groups.getGroupMembers(group);
-		logger.info("Group {}: action {}, {}", group, action, members);
-		//return null;
+
+		// modify code below to control plugs by publishing messages to MQTT broker
+		//List<String> members = groups.getGroupMembers(group);
 		Object ret = makeGroup(group);
+		//logger.info("Group {}: action {}, {}", group, action, members);
 		return ret;
 	}
-    @PostMapping("/api/groups/{group}")
+	@PostMapping("/api/groups/{group}")
 	public void createGroup(
 		@PathVariable("group") String group,
 		@RequestBody List<String> members) {
 		groups.setGroupMembers(group, members);
 		logger.info("Group {}: created {}", group, members);
-    }
-    @DeleteMapping("/api/groups/{group}")
+	}
+	@DeleteMapping("/api/groups/{group}")
 	public void removeGroup(
 		@PathVariable("group") String group) {
 		groups.removeGroup(group);
 		logger.info("Group {}: removed", group);
 	}
-    protected Object makeGroup(String group) {
+	protected Object makeGroup(String group) {
 		// modify code below to include plug states
 		HashMap<String, Object> ret = new HashMap<>();
 		ret.put("name", group);
-		HashMap<String,String> mem=new HashMap<>();
-		for (String member:groups.getGroupMembers(group))
-			{
-				//mem.put("name",member);
-				//mem.put("state",mqttController.getState(member));
-				mem.put(member,mqttController.getState(member));
-				
-			}
-		ret.put("members",mem);
+		ret.put("state",groups.getGroupState(group));
+		ret.put("members", groups.getGroupMembers(group));
 		return ret;
 	}
 
-		private static final Logger logger = LoggerFactory.getLogger(GroupsResource.class);	
+	private static final Logger logger = LoggerFactory.getLogger(GroupsResource.class);	
 }
