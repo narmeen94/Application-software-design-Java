33712504d6bf8745169e3c2ecccbe36c4b5a5c41
narmeen aamir <naamir@hawk.iit.edu>
2022-04-12 12:11:11 -0500

    all cases passed

--- src/main/java/ece448/iot_hub/GroupsResource.java
+++ src/main/java/ece448/iot_hub/GroupsResource.java
@@ -1,117 +1,6 @@
  package ece448.iot_hub;
 
- //version 1
-
-// import java.util.ArrayList;
-// import java.util.Collection;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.TreeMap;
-// import java.util.concurrent.SynchronousQueue;
-
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-// import ece448.grading.GradeP3.MqttController;
-// @RestController
-// public class GroupsResource {
-
-//      private final GroupsModel groups;
-// 	 private MqttController mqttController;
-     
-
-// 	public GroupsResource(GroupsModel groups,MqttController mqttController) {
-//         this.groups = groups;
-//         this.mqttController=mqttController;
-// 	}
-	
-// 	@GetMapping("/api/groups")
-// 	public Collection<Object> getGroups() throws Exception {
-// 		ArrayList<Object> ret = new ArrayList<>();
-// 		for (String group: groups.getGroups()) {
-// 			ret.add(makeGroup(group));
-// 		}
-// 		logger.info("Groups: {}", ret);
-// 		return ret;
-// 	 }
-
-//     @GetMapping("/api/groups/{group}")
-// 	public Object getGroup(
-// 		@PathVariable("group") String group,
-// 		@RequestParam(value = "action", required = false) String action) {
-// 		// if (action == null) {
-// 		// 	Object ret = makeGroup(group);
-// 		// 	logger.info("Group {}: {}", group, ret);
-// 		// 	return ret;
-// 		// }
-// 		if (action!=null)
-// 		{
-//             if(action.equals("on")||action.equals("off")||action.equals("toggle")){
-// 				HashMap<String, Object> ret = new HashMap<>();
-// 		        ret.put("name", group);
-// 		         HashMap<String,String> mem=new HashMap<>();
-// 		        for (String member:groups.getGroupMembers(group))
-// 			    {
-// 					mqttController.publishAction(member, action);
-// 				//mem.put("name",member);
-// 				//mem.put("state",mqttController.getState(member));
-// 				mem.put(member,mqttController.getState(member));
-				
-// 			    }
-// 		        ret.put("members",mem);
-		        
-
-//                //mqttController.publishAction(plugName, action);
-//             }
-//         }
-//         // modify code below to control plugs by publishing messages to MQTT broker
-// 		List<String> members = groups.getGroupMembers(group);
-// 		logger.info("Group {}: action {}, {}", group, action, members);
-// 		//return null;
-// 		Object ret = makeGroup(group);
-// 		return ret;
-// 	}
-//     @PostMapping("/api/groups/{group}")
-// 	public void createGroup(
-// 		@PathVariable("group") String group,
-// 		@RequestBody List<String> members) {
-// 		groups.setGroupMembers(group, members);
-// 		logger.info("Group {}: created {}", group, members);
-//     }
-//     @DeleteMapping("/api/groups/{group}")
-// 	public void removeGroup(
-// 		@PathVariable("group") String group) {
-// 		groups.removeGroup(group);
-// 		logger.info("Group {}: removed", group);
-// 	}
-//     protected Object makeGroup(String group) {
-// 		// modify code below to include plug states
-// 		HashMap<String, Object> ret = new HashMap<>();
-// 		ret.put("name", group);
-// 		HashMap<String,String> mem=new HashMap<>();
-// 		for (String member:groups.getGroupMembers(group))
-// 			{
-// 				//mem.put("name",member);
-// 				//mem.put("state",mqttController.getState(member));
-// 				mem.put(member,mqttController.getState(member));
-				
-// 			}
-// 		ret.put("members",mem);
-// 		return ret;
-// 	}
-
-// 		private static final Logger logger = LoggerFactory.getLogger(GroupsResource.class);	
-// }
-
 //version 2
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -213,16 +102,14 @@ public class GroupsResource {
 		//ret.put("members", groups.getGroupMembers(group));
 		return ret;
 	}
-	protected Object addGroup(String group) {
-		//PlugsResource plugRes=new PlugsResource(mqttController);
-		// modify code below to include plug states
-		HashMap<String, Object> retgroup = new HashMap<>();
-		retgroup.put("name", group);
-		retgroup.put("state",groups.getGroupState(group));
-		//ret.put("state",groups.getGroupState(group));
-		//ret.put("members", groups.getGroupMembers(group));
-		return retgroup;
-	}
+	// protected Object addGroup(String group) {
+		
+	// 	HashMap<String, Object> retgroup = new HashMap<>();
+	// 	retgroup.put("name", group);
+	// 	retgroup.put("state",groups.getGroupState(group));
+		
+	// 	return retgroup;
+	// }
 
 	private static final Logger logger = LoggerFactory.getLogger(GroupsResource.class);	
 }
